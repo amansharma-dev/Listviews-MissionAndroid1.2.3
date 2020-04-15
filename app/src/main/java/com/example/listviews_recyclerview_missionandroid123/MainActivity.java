e30c9895cb1569c0d70ef87de4fc505cf671d9e2
amansharma-dev <amansharma.11@hotmail.com>
2020-04-16 03:33:28 +0530

    commit

--- app/src/main/java/com/example/listviews_recyclerview_missionandroid123/MainActivity.java
+++ app/src/main/java/com/example/listviews_recyclerview_missionandroid123/MainActivity.java
@@ -4,19 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.listviews_recyclerview_missionandroid123.Data.DatabaseHandler;
 import com.example.listviews_recyclerview_missionandroid123.Model.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private ArrayList<String> contactArrayList;
+    private ArrayAdapter<String> contactArrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listView = findViewById(R.id.listView);
+        contactArrayList = new ArrayList<>();
         DatabaseHandler databaseHandler = new DatabaseHandler(MainActivity.this);
 
         //adding  entries, comment them after 1st run;
@@ -39,12 +49,27 @@ public class MainActivity extends AppCompatActivity {
 //        databaseHandler.addContact(new Contact("robbin","robb@yahoo.com","5410006350046"));
 
         //get all entries
-        List<Contact> contactList = databaseHandler.getAllContacts();
+        final List<Contact> contactList = databaseHandler.getAllContacts();
         for (Contact contact : contactList){
-            Log.d("GETALLCONTACTS", "onCreate: "+"Id: "+contact.getId()
-                    +",Name: "+contact.getName()
-                    + ", Email Id: "+contact.getEmailId()
-                    + ", Mobile Number: "+contact.getMobileNumber());
+            Log.d("GETALLCONTACTS", "onCreate: "+"Name: "+contact.getName());
+            contactArrayList.add(contact.getName());
         }
+
+        // create array adapter;
+        contactArrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                contactArrayList);
+
+        //add to list view
+        listView.setAdapter(contactArrayAdapter);
+
+        //attach eventListener to listView
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Log.d("ITEMCLICKED", "onItemClick: "+contactArrayList.get(position));
+            }
+        });
     }
 }
