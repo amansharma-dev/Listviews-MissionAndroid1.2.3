13612bffade26bccb479b3d1474a1ec8f68db84d
amansharma-dev <amansharma.11@hotmail.com>
2020-04-16 01:43:53 +0530

    commit

---
+++ app/src/main/java/com/example/listviews_recyclerview_missionandroid123/Data/DatabaseHandler.java
@@ -0,0 +1,25 @@
+package com.example.listviews_recyclerview_missionandroid123.Data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+
+    public DatabaseHandler(Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
---
+++ app/src/main/java/com/example/listviews_recyclerview_missionandroid123/Util/Util.java
@@ -0,0 +1,4 @@
+package com.example.listviews_recyclerview_missionandroid123.Util;
+
+public class Util {
+}
